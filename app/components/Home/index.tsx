dd783fc1b1d9ca03bbd45bcdc1579df133cc38a9
Isaac Lee <gssisaac@gmail.com>
2020-07-23 19:25:39 +0900

    color

--- app/components/Home/index.tsx
+++ app/components/Home/index.tsx
@@ -7,11 +7,13 @@ type Props = {
 export default function Home(props: Props) {
   return (
     <Container>
-      hello world
+      <p>Hello</p>
     </Container>
   )
 }
 
 export const Container = styled.div`
+  color: white;
+  background-color: #322322;
   height: 100vh;
 `
