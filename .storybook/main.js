79248c1ee2099dc28c5d0d79ad0c9de0310f631b
Isaac Chaneel Lee <gssisaac@gmail.com>
2020-07-28 10:53:06 +0900

    Update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,47 +1,48 @@
+
 const path = require('path')
 
 module.exports = {
-	stories: ["../app/**/*.stories.tsx"],
-	addons: [
-		"@storybook/addon-actions"
-	],
-	webpackFinal: (config) => {
-		const rules = [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader'
-			},
-			{
-				test: /\.css$/,
-				use: [
-					"style-loader",
-					"css-loader",
-				]
-			},
-			{
-				test: /\.(png|jpg|svg)$/,
-				use: {
-					loader: "file-loader"
-				}
-			},
-			{
+  stories: ['../app/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-actions',
+  ],
+  webpackFinal: (config) => {
+    const rules = [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
           'css-loader',
         ],
-			},
-		]
-		
-		rules.forEach((rule) => config.module.rules.push(rule))
-		config.resolve = {
-			extensions: ['.js', '.ts', '.tsx', '.json'],
-		}
-		config.node = {
-			fs: 'empty'
-		}
-		delete config.optimization
-		delete config.performance
-		return config
-	}
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        use: {
+          loader: 'file-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ]
+
+    rules.forEach((rule) => config.module.rules.push(rule))
+    config.resolve = {
+      extensions: ['.js', '.ts', '.tsx', '.json'],
+    }
+    config.node = {
+      fs: 'empty',
+    }
+    delete config.optimization
+    delete config.performance
+    return config
+  },
 }
