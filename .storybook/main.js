7240e14293ccb51ccf2cd6f2daeea0c65fe05229
jebae <bjmm9301@gmail.com>
2020-07-28 10:16:06 +0900

    add storybook setting

---
+++ .storybook/main.js
@@ -0,0 +1,47 @@
+const path = require('path')
+
+module.exports = {
+	stories: ["../app/**/*.stories.tsx"],
+	addons: [
+		"@storybook/addon-actions"
+	],
+	webpackFinal: (config) => {
+		const rules = [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader'
+			},
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					"css-loader",
+				]
+			},
+			{
+				test: /\.(png|jpg|svg)$/,
+				use: {
+					loader: "file-loader"
+				}
+			},
+			{
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+			},
+		]
+		
+		rules.forEach((rule) => config.module.rules.push(rule))
+		config.resolve = {
+			extensions: ['.js', '.ts', '.tsx', '.json'],
+		}
+		config.node = {
+			fs: 'empty'
+		}
+		delete config.optimization
+		delete config.performance
+		return config
+	}
+}
